870fb3f54645904ff096d8346c8a6e8d209db6a5
l1k4skr <82717429+l1k4skr@users.noreply.github.com>
2024-02-06 12:09:21 -0300

    Refactor form submit button and add event listeners

--- scripts.js
+++ scripts.js
@@ -1,28 +1,42 @@
+document.addEventListener('DOMContentLoaded', function(){
+    function new_task(){
+        const ul = document.querySelector('#Lista');
+        const text = document.querySelector('input[type="text"]');
 
-function new_task(){
-    const ul = document.querySelector('#Lista');
-    const li = document.createElement('li');
-    const checkbox = document.createElement('input');
-    const text = document.querySelector('input[type="text"]').value;
-    checkbox.type = 'checkbox';
-    li.appendChild(checkbox);
-    li.appendChild(document.createTextNode(text));
-    ul.appendChild(li);
-    
+        if (text.value === '') {
+            return;
+        }
+        const li = document.createElement('li');
+        const checkbox = document.createElement('input');
+        
+        checkbox.type = 'checkbox';
 
-}
+        li.appendChild(checkbox);
+        li.appendChild(document.createTextNode(text.value));
+        ul.appendChild(li);
+        console.log("Me estoy ejecutando")
+        text.value = '';
+        console.log(text)
+    }
 
-function remove_task(event){
-    console.log('remove task');
-}
+    function remove_task(){
+        const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        const ul = document.querySelector('#Lista');
+        for (let i = 0; i < checkboxes.length; i++) {
+            if (checkboxes[i].checked) {
+                ul.removeChild(checkboxes[i].parentNode);
+            }
+        }
+    }
 
-function add_task(event){
-    console.log('add task');
-}
+    function add_task(event){
+        console.log('add task');
+    }
 
-
-
-const form = document.querySelector('input[type="submit"]');
-form.addEventListener('click', new_task);
-
-console.log(form);
+    const Button = document.querySelector('#agregar');
+    Button.addEventListener('click', new_task);
+    if (document.querySelectorall('input[type="checkbox"]') !== null) {
+        const checkbox = document.querySelector('input[type="checkbox"]');
+        checkbox.addEventListener('click', remove_task);
+    }
+});
